1b9807748c174f59f89fbca3d73585f456f6192c
Imre Rad <radimre83@gmail.com>
2023-07-18 20:41:22 +0200

    timeout around ffmpeg as it sometimes gets stuck and never exits

--- getdigits.sh
+++ getdigits.sh
@@ -8,5 +8,5 @@ fi
 
 set -e
 dir="$(dirname $0)"
-ffmpeg -rtsp_transport tcp -i "$CAMURL" -vf "select=eq(pict_type\\,I)" -frames:v 1 "$pic" >/dev/null 2>&1
+timeout 10 ffmpeg -rtsp_transport tcp -i "$CAMURL" -vf "select=eq(pict_type\\,I)" -frames:v 1 "$pic" >/dev/null 2>&1
 "$dir/ocr.py" "$pic"
